3cc1c2230d1a85efb7f6a2615a3c320c88ece957
Inna Bondarenko <inna.bondarenko@codeit.pro>
2022-06-25 23:57:56 +0300

    Add config.js with variables for API

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export const key = 'cd3699c761a5e911cae4040d0707d8c4'; // https://api.themoviedb.org/3/movie/550?api_key=cd3699c761a5e911cae4040d0707d8c4
+export const apiUrl = 'https://api.themoviedb.org/3/';
+export const imageUrl = 'http://image.tmdb.org/t/p/original';
+export const apiRequestUrl = `${apiUrl}movie/`;
+export const apiRequestGenresUrl = `${apiUrl}genre/movie/list`;
+export const apiRequestTrendsUrl = `${apiUrl}trending/movie/day`;
+export const apiRequestSearchUrl = `${apiUrl}search/movie`;
