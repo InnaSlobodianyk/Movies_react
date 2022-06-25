37f0607e34102d902302c25b1b7bcd7f120279a9
Inna Bondarenko <inna.bondarenko@codeit.pro>
2022-06-25 23:59:20 +0300

    Add Trendcard component

---
+++ src/components/Trendcard/Trendcard.js
@@ -0,0 +1,99 @@
+import trendcardClass from "./Trendcard.module.scss";
+import { imageUrl } from "../../config";
+import { HiOutlineBookmark } from "react-icons/hi";
+// import { HiBookmark } from "react-icons/hi";
+
+const Trendcard = ( { movie } ) => {
+    console.log('%c PROPS' , 'background: #222; color: #bada55');
+    console.log(movie);
+
+    const calcDate = releaseDate => {
+        if (releaseDate !== '') {
+            const date = new Date(releaseDate);
+            return date.getFullYear();
+        } else {
+            return '';
+        }
+    };
+
+    const limitMovieTitle = ( title, limit = 28 ) => {
+        const newTitle = [];
+
+        if(title.length > limit) {
+            title.split(' ').reduce((acc, cur) => {
+
+                if(acc + cur.length <= limit) {
+                    newTitle.push(cur);
+                }
+
+                return acc + cur.length;
+            }, 0);
+
+            return `${newTitle.join(' ')}...`;
+        }
+
+        return title;
+    };
+
+    return (
+        <figure className={trendcardClass['item']} id={ `trendcard-${movie.id}`}>
+            <div className={trendcardClass['thumb']}>
+                <img src={imageUrl + movie.poster_path}
+                     alt={movie.title}
+                     className={trendcardClass['img']} />
+                <div className={trendcardClass['rating']}>
+                    <span>{movie.vote_average}</span>
+                </div>
+
+                <div className={trendcardClass['description']}>
+                    <div className={trendcardClass['description-heading']}>
+                        <div className={trendcardClass['info']}>
+                            <a href={`/movie/#${movie.id}`} className={trendcardClass['permalink']}>
+                                <h3 className={trendcardClass['description-title']}>
+                                    {movie.title}
+                                </h3>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className={trendcardClass['description-content']}>
+                        <p>
+                            {movie.overview}
+                        </p>
+                    </div>
+
+                    <a href={`/movie/#${movie.id}`} className="btn btn btn-primary">Details</a>
+                </div>
+            </div>
+
+            <figcaption className={trendcardClass['heading']}>
+                <div className={trendcardClass['info-wrapper']}>
+                    <div className={trendcardClass['info']}>
+                        <a href={`/movie/#${movie.id}`} className={trendcardClass['permalink']}>
+                            <h3 className={[trendcardClass['title'], trendcardClass['title-cropped']].join(' ')}>
+                                { limitMovieTitle(movie.title) }
+                            </h3>
+                        </a>
+                        <span className={trendcardClass['release']}>
+                            { calcDate(movie.release_date) }
+                        </span>
+                    </div>
+
+                    <div className={trendcardClass['info-details']}>
+                        <button className={trendcardClass['icon']} data-hash={movie.id}>
+                            <HiOutlineBookmark className={trendcardClass['icon-svg']} />
+                        </button>
+                    </div>
+                </div>
+
+                <ul className={trendcardClass['genres']}>
+                    <li className={trendcardClass['genres-item']}>Genre 1</li>
+                    <li className={trendcardClass['genres-item']}>Genre 2</li>
+                    <li className={trendcardClass['genres-item']}>Genre 3</li>
+                </ul>
+            </figcaption>
+        </figure>
+    );
+}
+
+export default Trendcard;
