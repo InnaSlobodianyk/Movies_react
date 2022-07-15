22a7704e93e665c54216b678f08e5f25879b081a
Inna Bondarenko <inna.bondarenko@codeit.pro>
2022-07-15 22:39:26 +0300

    Add routes to header, replace Header component to /layout/ folder, add styles to Header.module.scss

---
+++ src/components/layout/Header.js
@@ -0,0 +1,34 @@
+import { Link, NavLink } from "react-router-dom";
+
+import classes from './Header.module.scss';
+import logo from "../../images/movierise-logo.png";
+
+const Header = () => {
+  return (
+    <header className={classes['header']}>
+      <div className={classes['header__container']}>
+        <Link to="/" className={classes['header__logo']}>
+          <img src={logo} alt="logo-movierise"/>
+        </Link>
+        <nav className={classes['header__menu-nav']} role="navigation">
+          <ul className={classes['header__menu']}>
+            <li className={classes['header__menu-item']}>
+              <NavLink to="/lists/" className={classes['header__menu-link']}>
+                <span className={classes['header__menu-link-name']}>Lists</span>
+              </NavLink>
+            </li>
+            <li className={classes['header__menu-item']}>
+              <NavLink to="/favourites/" className={classes['header__menu-link']}>
+                <i className="fa fa-bookmark"/>
+                <span className={classes['header__menu-link-name']}>Favorites</span>
+                <span className={classes['header__menu-num']}/>
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
