f80d71770b06cffb1b10459907dcbecc7f66609d
Inna Bondarenko <inna.bondarenko@codeit.pro>
2022-07-15 22:40:54 +0300

    Add placeholders to Favourites, Lists, NotFound components

---
+++ src/pages/Favourites.js
@@ -0,0 +1,5 @@
+const Favourites = () => {
+  return <h1>Favourites page</h1>;
+}
+
+export default Favourites;
---
+++ src/pages/Lists.js
@@ -0,0 +1,5 @@
+const Lists = () => {
+  return <h1>Lists page</h1>;
+}
+
+export default Lists;
