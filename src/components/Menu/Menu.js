e67c5740c8cd00a7d66068b4fe494b0684b9ac80
Inna Bondarenko <inna.bondarenko@codeit.pro>
2022-08-15 11:24:54 +0300

    Add Menu component

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router-dom";
+
+import styles from "./Menu.module.scss";
+
+const menu = [
+  {
+    url: '/lists/',
+    title: 'Lists'
+  },
+  {
+    url: '/favorites/',
+    title: 'Favorites'
+  },
+];
+
+const Menu = () => {
+  return (
+    <nav className={styles.menuNav} role="navigation">
+      <ul className={styles.menu}>
+        {menu.map((menuItem) => (
+          <li key={menuItem.title} className={styles.menuItem}>
+            <NavLink to={menuItem.url} className={styles.menuLink}>
+              <span className={styles.menuLinkName}>{menuItem.title}</span>
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Menu;
