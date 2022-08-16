a959e2bc8160ed5c925c6130aa83b1647cb3e770
Inna Bondarenko <inna.bondarenko@codeit.pro>
2022-08-16 23:57:04 +0300

    Fix Button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,8 +3,15 @@ import cn from "classnames";
 import styles from "./Button.module.scss";
 
 const Button = ( props ) => {
+    let buttonClasses;
+    if (props.className) {
+        buttonClasses = cn(styles.btn, props.className)
+    } else {
+        buttonClasses = styles.btn
+    }
+
     return (
-        <button className={cn(styles.btn, props.className)}>
+        <button className={buttonClasses}>
             { props.children }
         </button>
     );
