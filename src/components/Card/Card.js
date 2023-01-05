76a799da3c1a730143a07fc0f606f19ef78db9e5
Inna Bondarenko <inna.bondarenko@codeit.pro>
2023-01-05 21:15:54 +0200

    Fix Card component and styles

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,59 +1,60 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { IoStar } from "react-icons/io5";
 import cn from "classnames";
 
 import { imageUrl } from "config";
-import { calcDate } from "helpers/helpers";
+import { calcDate, limitMovieTitle, roundRatingValue } from "helpers/helpers";
 import Genre from "components/Genre/Genre";
 
 import styles from "./Card.module.scss";
 
-const Card = ( props ) => {
-  console.log(props);
+const Card = ({ props }) => {
   const [item, setItem] = useState();
-  // const [releaseDate, setReleaseDate] = useState();
 
   useEffect(() => {
     setItem(props)
-    // setReleaseDate(calcDate(props.release_date));
 
     return () => {};
   }, [item, props]);
-  console.log('%c ITEM' , 'background: #222; color: #bada55');
-  console.log(item);
 
-  console.log('%c 1111111' , 'background: #222; color: #bada55');
-  console.log(item);
+  if(!item) return <></>;
 
-  // if(!item && !item.props) return <></>;
+  const ratingRounded = roundRatingValue(item.vote_average);
 
   return (
     <div className={cn(styles.card, props.className)}>
-      {/*{props.children}*/}
-      {/*<figure className={styles.card__imgWrapper}>*/}
-      {/*  <img src={item.props.poster_path && `${imageUrl}/${item.props.poster_path}`}*/}
-      {/*       className={styles.card__img} alt="" />*/}
+      <Link to={`/movie/${item.id}`}>
+        <figure className={styles.card__imgWrapper}>
+          <img src={item.poster_path && `${imageUrl}/${item.poster_path}`}
+               className={styles.card__img} alt="" />
+        </figure>
+      </Link>
 
-      {/*  {item.props.genre_ids && (*/}
-      {/*    <figcaption className={styles.card__caption}>*/}
-      {/*      <Genre className={styles.card__genres} genres={item.props.genre_ids} />*/}
-      {/*    </figcaption>*/}
-      {/*  )}*/}
-      {/*</figure>*/}
+      <div className={styles.card__description}>
+        <Link to={`/movie/${item.id}`} className={styles.card__link}>
+          <span className={styles.card__title}>{limitMovieTitle(item.title)}</span>
+        </Link>
 
-      {/*<div className={styles.card__description}>*/}
-      {/*  <Link to={`/movie/${item.props.id}`} className={styles.card__link}>*/}
-      {/*    <span className={styles.card__title}>{item.props.title}</span>*/}
-      {/*  </Link>*/}
+        <div className={styles.card__details}>
+          {item.genre_ids && (
+            <Genre genres={item.genre_ids} />
+          )}
 
-      {/*  <div className={styles.card__content}>*/}
-      {/*    /!*<div className={styles.card__release}>{releaseDate}</div>*!/*/}
+          <div className={styles.card__content}>
+            {item.release_date && (
+              <div className={styles.card__release}>{ calcDate(item.release_date) }</div>
+            )}
 
-      {/*    <div className={styles.card__ratingValue}>*/}
-      {/*      6.8*/}
-      {/*    </div>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
+            {item.vote_average && (
+              <div className={styles.card__ratingValue}>
+                <IoStar className={styles.card__ratingStar} />
+                {ratingRounded}
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
