bc52508f8c6ca3131508f90ae7b60e77fc28b929
Inna Bondarenko <inna.bondarenko@codeit.pro>
2023-01-05 21:12:18 +0200

    Add option to Label component, add styles

--- src/components/Label/Label.js
+++ src/components/Label/Label.js
@@ -4,7 +4,7 @@ import styles from "./Label.module.scss";
 
 const Label = ( props ) => {
   return (
-    <div className={cn(styles.label, props.className)}>
+    <div className={cn(styles.label, props.variant && props.variant === 'plain' ? styles.plain : styles.gradient, props.className)}>
       {props.children}
     </div>
   );
