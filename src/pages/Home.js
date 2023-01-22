099adb10468ed1f672a813bad8dc86940d7cff5b
Inna Bondarenko <inna.bondarenko@codeit.pro>
2023-01-22 14:57:12 +0200

    Add Carousel component homepage, change layout depends on location

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,27 +4,39 @@ import cn from "classnames";
 import { getTrends } from "services/trends";
 
 import Trendcard from "components/Trendcard/Trendcard";
+import { getPopulars } from "services/populars";
+
+import Carousel from "components/Carousel/Carousel";
 
 import styles from "components/layout/Layout.module.scss";
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
+  const [popularMovies, setPopularMovies] = useState([]);
 
   useEffect(() => {
     getTrends().then((response) => {
       setMovies(response);
     });
+
+    getPopulars().then((response) => {
+      setPopularMovies(response);
+    });
   }, []);
 
   return (
     <>
-      <h2 className={cn(styles.pageHeading, styles['pageHeading--2'])}>Trending movies</h2>
+      { popularMovies && <Carousel slides={ popularMovies } /> }
 
-      { movies && (
-        <div className={styles.container}>
-          { movies.map( movie => <Trendcard key={ movie.id } movie={ movie } /> ) }
-        </div>
-      ) }
+      <div className={ styles.pageContainer }>
+        <h2 className={ cn( styles.pageHeading, styles['pageHeading--2'] ) }>Trending movies</h2>
+
+        { movies && (
+          <div className={styles.container}>
+            { movies.map( movie => <Trendcard key={ movie.id } movie={ movie } /> ) }
+          </div>
+        ) }
+      </div>
     </>
   )
 };
