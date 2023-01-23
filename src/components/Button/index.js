fed91c5d573139c9bc4c803ba469416328761bc6
Inna Bondarenko <inna.bondarenko@codeit.pro>
2023-01-23 13:17:40 +0200

    Update Button component

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,11 +2,11 @@ import cn from "classnames";
 
 import styles from "./Button.module.scss";
 
-const Button = ({ type = 'button', disabled = false, className, children }) => {
+const Button = ({ type = 'button', disabled = false, className, onClick, children }) => {
     const buttonClasses = cn(styles.btn, className);
 
     return (
-        <button className={buttonClasses} type={type} disabled={disabled}>
+        <button className={buttonClasses} type={type} disabled={disabled} onClick={ onClick }>
             { children }
         </button>
     );
