35cfe809dd539ac044eabb329cbc19159b0cca3e
Inna Bondarenko <inna.bondarenko@codeit.pro>
2023-01-23 13:21:21 +0200

    Add SearchForm component to Header component, update styles

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,19 +1,44 @@
+import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import Menu from "components/Menu";
+
+import SearchForm from "components/SearchForm/SearchForm";
+
+import { searchActions } from "store";
 
 import logo from "assets/images/movierise-logo.png";
 
 import styles from './Header.module.scss';
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const showSearchResults = useSelector(( state ) => state.search.showSearchResults);
+
+  const clickHandler = (e) => {
+      e.preventDefault();
+
+      if(showSearchResults) {
+        dispatch(searchActions.showSearchResults(false));
+        dispatch(searchActions.searchQuery(''));
+        dispatch(searchActions.setSearchResults([]));
+        dispatch(searchActions.setTotalPages(0));
+        dispatch(searchActions.setTotalResults(0));
+      }
+
+  };
+
   return (
-    <header className={styles.header}>
-      <div className={styles.headerContainer}>
-        <Link to="/" className={styles.headerLogo}>
+    <header className={ styles.header }>
+      <div className={ styles.headerContainer }>
+        <Link to="/" className={ styles.headerLogo }>
           <img src={ logo } alt="logo-movierise"/>
         </Link>
 
-        <Menu />
+        <div className={ styles.headerMenuContainer }>
+          <SearchForm  />
+
+          <Menu />
+        </div>
       </div>
     </header>
   );
