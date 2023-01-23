fba7e20f4efc4b8b287b355f5c0983f82b0b9c9a
Inna Bondarenko <inna.bondarenko@codeit.pro>
2023-01-23 13:19:35 +0200

    Update App component to use store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from "react-router-dom";
 
-import './index.css';
+import store from "./store";
 import App from './App';
 
+import './index.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+
+const rerenderApp = () => {
+  root.render(
+    <Provider store={ store }>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+};
+
+rerenderApp(store.getState());
+
+store.subscribe(rerenderApp);
