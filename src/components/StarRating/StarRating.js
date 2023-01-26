1b56c23890dbc227d49aeebc9d0f11935030d6ca
Inna Bondarenko <inna.bondarenko@codeit.pro>
2023-01-26 19:22:07 +0200

    Fix StarRating component

--- src/components/StarRating/StarRating.js
+++ src/components/StarRating/StarRating.js
@@ -1,39 +1,46 @@
+import React, { useMemo } from "react";
+
 import { IoStar, IoStarOutline } from "react-icons/io5";
-import { calcRatingWidth, roundRatingValue } from "helpers/helpers";
+import { calcRatingWidth, roundRatingValue } from "helpers";
 import Label from "components/Label/Label";
 
 import styles from "./StarRating.module.scss";
 
+const starsNum = [...Array(10)];
+
 const Stars = ({ isOutline }) => {
   return (
     <>
-      {[...Array(10)].map((star, index) => {
-        return isOutline ? <IoStarOutline key={index} /> : <IoStar key={index} />;
-      })}
+      { starsNum.map( (star, index) => {
+        return isOutline ? <IoStarOutline key={ index } /> : <IoStar key={ index } />;
+      } ) }
     </>
   );
 };
 
 const StarRating = ({ rating }) => {
+  const ratingWidth = useMemo(() => calcRatingWidth(rating), [rating]);
+  const ratingValue = useMemo(() => roundRatingValue(rating), [rating]);
+
   return (
-    <div className={styles.rating}>
-      <div className={styles.rating__inner}>
-        <div className={styles.rating__base}>
+    <div className={ styles.rating }>
+      <div className={ styles.rating__inner }>
+        <div className={ styles.rating__base }>
           <Stars isOutline />
         </div>
 
-        <div className={styles.rating__active} style={{width: `${calcRatingWidth(rating)}`}}>
+        <div className={ styles.rating__active } style={{width: `${ ratingWidth }`}}>
           <Stars />
         </div>
       </div>
 
       <Label>
-        <span className={styles.rating__label}>
-          {roundRatingValue(rating)}
+        <span className={ styles.rating__label }>
+          { ratingValue }
         </span>
       </Label>
     </div>
   );
 };
 
-export default StarRating;
+export default React.memo(StarRating);
