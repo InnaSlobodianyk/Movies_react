c5bb4db05e776cdc413b83d0d22b6f0076dff5e0
Inna Bondarenko <inna.bondarenko@codeit.pro>
2023-01-31 13:59:17 +0200

    Replace the logic of getting trending movies to Home component from TrendcardContainer conponent

--- src/components/TrendcardsContainer/TrendcardsContainer.js
+++ src/components/TrendcardsContainer/TrendcardsContainer.js
@@ -1,19 +1,8 @@
-import React, { useEffect, useState } from "react";
-import { getTrends } from "services/trends";
-
 import Trendcard from "components/Trendcard/Trendcard";
 
 import styles from "./TrendcardsContainer.module.scss";
 
-const TrendcardsContainer = () => {
-  const [movies, setMovies] = useState([]);
-
-  useEffect(() => {
-    getTrends().then((response) => {
-      setMovies(response);
-    });
-  }, []);
-
+const TrendcardsContainer = ({ movies }) => {
   return (
     <div className={styles.container}>
       { movies.map( movie => <Trendcard key={movie.id} movie={movie}/> ) }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,26 @@
+import React, { useEffect, useState } from "react";
 import cn from "classnames";
+
+import { getTrends } from "services/trends";
 
 import TrendcardsContainer from "components/TrendcardsContainer/TrendcardsContainer";
 
 import styles from "components/layout/Layout.module.scss";
 
 const Home = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    getTrends().then((response) => {
+      setMovies(response);
+    });
+  }, []);
+
   return (
     <>
       <h2 className={cn(styles.pageHeading, styles['pageHeading--2'])}>Trending movies</h2>
-      <TrendcardsContainer />
+
+      { movies && <TrendcardsContainer movies={ movies } /> }
     </>
   )
 };
