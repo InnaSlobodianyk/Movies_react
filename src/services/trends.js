f0610b7465ae383b5e69fb08ee8567d0f9aa6a5c
Inna Bondarenko <inna.bondarenko@codeit.pro>
2023-02-03 00:51:32 +0200

    Add getAllGenres request to trends service, prepare movies objects to return

--- src/services/trends.js
+++ src/services/trends.js
@@ -1,17 +1,30 @@
 import { sendRequest } from "./apiService";
 import { apiRequestTrendsUrl } from "config";
+import { getAllGenres } from "./genres";
+import { filterGenres, roundRatingValue } from "helpers";
 
 export const getTrends = async () => {
   try {
-    const response = await sendRequest({
-      url: apiRequestTrendsUrl,
-      params: {
-        page: 1,
-        language: 'en'
-      },
-    });
+    const [trends, allGenres] = await Promise.all([
+      sendRequest({
+        url: apiRequestTrendsUrl,
+        params: {
+          page: 1,
+          language: 'en'
+        },
+      }),
 
-    return response.results;
+      getAllGenres()
+    ]);
+
+    const movies = trends.results.map( movie => {
+      const filteredGenres = filterGenres(allGenres, movie.genre_ids);
+      const ratingRounded = roundRatingValue(movie.vote_average);
+
+      return { ...movie, vote_average: ratingRounded, genres: filteredGenres }
+    } );
+
+    return movies;
   } catch (e) {
     return [];
   }
