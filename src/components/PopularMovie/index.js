fd4fda1be4c84515ba5588daa8381bd6bcbd5eff
Inna Bondarenko <inna.bondarenko@codeit.pro>
2023-02-06 22:56:03 +0200

    Add PopularMovie component with styles

---
+++ src/components/PopularMovie/index.js
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+
+import Label from "components/Label/Label";
+import Genre from "components/Genre/Genre";
+import StarRating from "components/StarRating/StarRating";
+
+import { imageFullUrl } from "helpers";
+
+import styles from "./PopularMovie.module.scss";
+
+const PopularMovie = ( { movieDetails } ) => (
+  <div
+    id={ movieDetails.id }
+    className={ styles.popularMovieItem }
+    style={{ backgroundImage: movieDetails.backdrop_path && `url(${ imageFullUrl({ imagePath: movieDetails.backdrop_path } ) }` }}
+  >
+    <div className={ styles.popularMovieHolder }>
+      <h2 className={ styles.popularMovieTitle }>{ movieDetails.title }</h2>
+
+      <div className={ styles.popularMovieInfo }>
+        <div className={ styles.popularMovieRelease }>
+          <Label variant='plain' >
+            { movieDetails.release_date }
+          </Label>
+        </div>
+
+        <Genre genres={ movieDetails.genres } variant='plain' labeled />
+      </div>
+
+      <p className={ styles.popularMovieOverview }>{ movieDetails.overview }</p>
+
+      <div className={ styles.popularMovieRating }>
+        { movieDetails.vote_average && <StarRating rating={ movieDetails.vote_average } /> }
+      </div>
+
+      <Link to={`/movie/${ movieDetails.id }`} className={styles.popularMovieBtn}>
+        <Label>
+          Details
+        </Label>
+      </Link>
+    </div>
+  </div>
+);
+
+export default PopularMovie;
