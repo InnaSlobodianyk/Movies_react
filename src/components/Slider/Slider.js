6f35b7f414c111f3805bb7af785d1347af9ede9a
Inna Bondarenko <inna.bondarenko@codeit.pro>
2023-02-06 22:56:58 +0200

    Update Slider component to display movie details on slider

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,34 +1,48 @@
 import cn from "classnames";
 
-import { Navigation, Pagination, A11y } from "swiper";
+import { Navigation, Pagination, Autoplay, A11y } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+import PopularMovie from "components/PopularMovie";
+
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
+import "swiper/scss/autoplay";
 
 import styles from "./Slider.module.scss";
 
-const Slider = ( { slides, navigation = true, pagination = false, className } ) => {
-  return (
-    <Swiper
-      navigation={ navigation }
-      pagination={ pagination }
-      modules={ [ Navigation, Pagination, A11y ] }
-      className={  cn( styles.slider, className ) }
-    >
-      { slides.map( ( item ) => (
-        <SwiperSlide
-          key={ item.key }
-          className={ styles.sliderItem }
-        >
+const Slider = (
+  {
+    slides,
+    navigation = true,
+    pagination = false,
+    videos = true,
+    autoplay, className
+  } ) => (
+  <Swiper
+    navigation={ navigation }
+    pagination={ pagination }
+    modules={ [ Navigation, Pagination, Autoplay, A11y ] }
+    autoplay={ autoplay }
+    className={ cn( styles.slider, className ) }
+  >
+    { slides.map( ( item ) => (
+      <SwiperSlide
+        key={ item.key || item.id }
+        className={ cn( videos ? styles.sliderItemVideo : styles.sliderItem ) }
+      >
+        { videos ? (
           <iframe width='560' height='315' src={`https://www.youtube.com/embed/${ item.key }`} frameBorder='0'
                   allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-                  allowFullScreen title={ item.name }></iframe>
-        </SwiperSlide>
-      ) ) }
-    </Swiper>
-  );
-};
+                  allowFullScreen title={ item.name } />
+        ) : (
+          <PopularMovie movieDetails={ item } />
+        ) }
+
+      </SwiperSlide>
+    ) ) }
+  </Swiper>
+);
 
 export default Slider;
