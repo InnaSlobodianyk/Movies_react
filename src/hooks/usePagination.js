02d8425c5aec01bc9a563d24b5a8f6759bf13b69
Inna Bondarenko <inna.bondarenko@codeit.pro>
2023-02-08 23:52:37 +0200

    Fix variables in usePagination.js

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -3,7 +3,7 @@ import { useMemo } from 'react';
 export const DOTS = '...';
 
 const range = ( start, end ) => {
-  let length = end - start + 1;
+  const length = end - start + 1;
 
   return Array.from({ length }, (_, idx) => idx + start);
 };
@@ -40,15 +40,15 @@ export const usePagination = ({ siblingCount = 1, currentPage, totalPages }) => 
     const lastPageIndex = totalPages;
 
     if (!shouldShowLeftDots && shouldShowRightDots) {
-      let leftItemCount = 3 + 2 * siblingCount;
-      let leftRange = range(1, leftItemCount);
+      const leftItemCount = 3 + 2 * siblingCount;
+      const leftRange = range(1, leftItemCount);
 
       return [...leftRange, DOTS, totalPages];
     }
 
     if (shouldShowLeftDots && !shouldShowRightDots) {
-      let rightItemCount = 3 + 2 * siblingCount;
-      let rightRange = range(
+      const rightItemCount = 3 + 2 * siblingCount;
+      const rightRange = range(
         totalPages - rightItemCount + 1,
         totalPages
       );
@@ -56,7 +56,7 @@ export const usePagination = ({ siblingCount = 1, currentPage, totalPages }) => 
     }
 
     if (shouldShowLeftDots && shouldShowRightDots) {
-      let middleRange = range(leftSiblingIndex, rightSiblingIndex);
+      const middleRange = range(leftSiblingIndex, rightSiblingIndex);
       return [firstPageIndex, DOTS, ...middleRange, DOTS, lastPageIndex];
     }
   }, [siblingCount, currentPage, totalPages]);
